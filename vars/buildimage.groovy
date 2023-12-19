db4eaeb06fc3f76b7be800cff967c4586cde8905
shashank <shashankhs5242@gmail.com>
2023-12-20 01:15:26 +0530

    sus

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,5 +1,6 @@
 def call() {
-    withCredentials([string(credentialsId: 'movieapi', variable: 'sus')]) {
+   def secrets= 'movieapi'
+    withCredentials([string(credentialsId: 'secrets', variable: 'sus')]) {
     
     sh """
       docker build --build-arg TMDB_V3_API_KEY=${sus} -t netflix .
