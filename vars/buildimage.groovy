bca6630a4c96c9983140aa3dfb376363b3b14573
shashank <shashankhs5242@gmail.com>
2023-12-20 01:28:13 +0530

    sus

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -3,7 +3,8 @@ def call() {
     withCredentials([string(credentialsId: 'TMDB_V3_API_KEY', variable: 'TMDB_V3_API_KEY')]) {
     
     sh """
-      docker build --build-arg TMDB_V3_API_KEY=${TMDB_V3_API_KEY} -t netflix .
+      docker rmi -f netflix
+      docker build --build-arg TMDB_V3_API_KEY=\$TMDB_V3_API_KEY -t netflix .
       docker run -d --name netflix -p 8081:80 netflix
     """
 }
