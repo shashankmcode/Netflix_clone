4e20cb7e0b91345a5e6530466839d6aa1db774b1
shashank <shashankhs5242@gmail.com>
2023-12-19 17:11:42 +0530

    sus

--- vars/sonarscan.groovy
+++ vars/sonarscan.groovy
@@ -1,6 +1,5 @@
 def call(){
     withSonarQubeEnv(credentialsId: 'sonar-api') {
-    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Netflix \
-                    -Dsonar.projectKey=Netflix '''
+    sh 'mvn clean package sonar:sonar'
 }
 }
